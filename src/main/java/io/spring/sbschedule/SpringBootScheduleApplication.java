d384fc184caba9de9bb50ba5bf5f31d5dff9fb41
JackTran791 <congsonit@gmail.com>
2018-10-26 21:15:31 -0500

    Initial commit

---
+++ src/main/java/io/spring/sbschedule/SpringBootScheduleApplication.java
@@ -0,0 +1,12 @@
+package io.spring.sbschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootScheduleApplication.class, args);
+    }
+}
